a5a2d430104d153447806edb5356ea3de73d9117
Maygn <Mayneyra87@gmail.com>
2025-03-19 23:55:50 +0100

    borrar cosas

--- Musica/src/main/java/musica/MusicaController.java
+++ Musica/src/main/java/musica/MusicaController.java
@@ -1,7 +1,6 @@
 package musica;
 
 import java.util.Base64;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,7 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
--- Musica/src/main/java/musica/MusicaService.java
+++ Musica/src/main/java/musica/MusicaService.java
@@ -1,6 +1,5 @@
 package musica;
 
-import java.io.IOException;
 import java.util.Base64;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,11 +9,6 @@ import org.apache.commons.codec.digest.HmacUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @Service
 public class MusicaService {
@@ -31,8 +25,16 @@ public class MusicaService {
 
     public String defaultJson() {
         // Genera un JSON por defecto para el campo 'musica'
-        String jsonString = "{ \"a1\": { \"11\": [\"string1\", \"string2\", \"string3\"], \"12\": [\"string4\", \"string5\", \"string6\"], \"13\": [\"string7\", \"string8\", \"string9\"], \"14\": [\"string10\", \"string11\", \"string12\"], \"15\": [\"string13\", \"string14\", \"string15\"] }, \"a2\": { \"21\": { \"211\": [\"String extra 1\", \"String extra 2\"], \"212\": [\"String extra doble 1\", \"String extra doble 2\"] }, \"22\": [\"string19\", \"string20\", \"string21\"], \"23\": [\"string22\", \"string23\", \"string24\"], \"24\": [\"string25\", \"string26\", \"string27\"], \"25\": [\"string28\", \"string29\", \"string30\"] }, \"a3\": { \"31\": [\"string31\", \"string32\", \"string33\"], \"32\": [\"string34\", \"string35\", \"string36\"], \"33\": [\"string37\", \"string38\", \"string39\"], \"34\": [\"string40\", \"string41\", \"string42\"], \"35\": [\"string43\", \"string44\", \"string45\"] }, \"a4\": { \"41\": [\"string46\", \"string47\", \"string48\"], \"42\": [\"string49\", \"string50\", \"string51\"], \"43\": [\"string52\", \"string53\", \"string54\"], \"44\": [\"string55\", \"string56\", \"string57\"], \"45\": [\"string58\", \"string59\", \"string60\"] }, \"a5\": [\"string61\", \"string62\", \"string63\"] }";
-        return jsonString;
+    	String jsonString = "{"
+    		    + " \"Boss\": [\"Malvado\", \"Antiheroe\", \"Asesino\"],"
+    		    + " \"Ciudad\": [\"Pobre\", \"Desierto\", \"Ladrones\", \"Steampunk\"],"
+    		    + " \"Exploracion\": [\"Ruinas\", \"Bosque\", \"Mazmorra\"],"
+    		    + " \"Combate\": [\"Taberna\", \"Campamento\", \"Plano Astral\"],"
+    		    + " \"Descanso\": [\"Nubes\", \"Sol\", \"Noche\"]"
+    		    + "}";     
+    	return jsonString;
+    		  
+    		    
     }
 
     public Musica guardarCuenta(String usuario, String defJson) {
